a31b5647ab5eb8f732954bef0ce07118fd73c3a7
Xuepoo (CI) <xuepoofoter@gmail.com>
2026-05-30 15:31:54 +0000

    chore: update sonic-bridge to v0.3.5

--- Formula/sonic-bridge.rb
+++ Formula/sonic-bridge.rb
@@ -1,22 +1,22 @@
 class SonicBridge < Formula
   desc "An ultra-fast, lightweight physical music aesthetic and listening translation middleware designed for AI Agents"
   homepage "https://github.com/Xuepoo/sonic-bridge"
-  version "0.3.4"
+  version "0.3.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.4/sonic-bridge-macos-aarch64"
-      sha256 "89273bde1863476cbd23d405bf4bd0fe9497919666680747e401c7cd9faceb8a"
+      url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.5/sonic-bridge-macos-aarch64"
+      sha256 "962248bccc98ac8ed56cfffae6f621cdcb9db43d7d2deeaa253685bc9616b687"
     else
-      url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.4/sonic-bridge-macos-x86_64"
-      sha256 "3577bef99022576c0a436f465df9df0481ff74de99b3c6c230baa6855f2cf8fb"
+      url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.5/sonic-bridge-macos-x86_64"
+      sha256 "9249eb801576aa504ca1a02a8167498b08dd505d05e6c3227765a755606f2b58"
     end
   end
 
   on_linux do
-    url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.4/sonic-bridge-linux-x86_64"
-    sha256 "af18ae9b03e0b46771f2c69425bddf9153333d33eedc2312a6c111aab2f33322"
+    url "https://github.com/Xuepoo/sonic-bridge/releases/download/v0.3.5/sonic-bridge-linux-x86_64"
+    sha256 "8460f2058ec95b4d7896689ac28b21288a5b192d73ba1cd05636593264e9908c"
   end
 
   def install

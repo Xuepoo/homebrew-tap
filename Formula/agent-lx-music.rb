7ba320d0f05974563eec913a05f8ad42f24203ac
Xuepoo (CI) <xuepoofoter@gmail.com>
2026-05-31 15:28:56 +0000

    chore: update agent-lx-music to v0.3.3

--- Formula/agent-lx-music.rb
+++ Formula/agent-lx-music.rb
@@ -1,22 +1,22 @@
 class AgentLxMusic < Formula
   desc "A terminal-native music CLI replacing lx-music-desktop, powered by Agentic intelligence"
   homepage "https://github.com/Xuepoo/agent-lx-music"
-  version "0.3.2"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.2/alx-macos-aarch64"
-      sha256 "3287a33e812b34a384ba7e51305122adefc192064431a2ff389763b12fc281d6"
+      url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.3/alx-macos-aarch64"
+      sha256 "faa7a52bf647d740385933525cfddd7d399d149959a6497a5bd62fa72917f808"
     else
-      url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.2/alx-macos-x86_64"
-      sha256 "fbe0f5c21b87b692f47177691ee1e5fb3e8a9faf0122ace0f6a7c9e1f349d0ab"
+      url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.3/alx-macos-x86_64"
+      sha256 "8b16d85a73334b49c7154f75786af37d5a679082ceee0b08792f419880f3ba8f"
     end
   end
 
   on_linux do
-    url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.2/alx-linux-x86_64"
-    sha256 "643d130ec154dac7b5fc344488212daa7858d82120103c00bc872e7423c19b2b"
+    url "https://github.com/Xuepoo/agent-lx-music/releases/download/v0.3.3/alx-linux-x86_64"
+    sha256 "210b4609b252a73cf46a27198d24e0f11572e063c67e9435d38cdc97516040bf"
   end
 
   def install

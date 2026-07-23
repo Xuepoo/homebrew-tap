515bedc806ff5a3f01c02132086891a62b422c72
Xuepoo <xuepoofoter@gmail.com>
2026-07-23 02:33:49 -0400

    feat(carryctx): add formula v0.0.1

---
+++ Formula/carryctx.rb
@@ -0,0 +1,46 @@
+class Carryctx < Formula
+  desc "Persistent project context for coding agents"
+  homepage "https://carryctx.dev"
+  version "0.0.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/Xuepoo/carryctx/releases/download/v0.0.1/carryctx-aarch64-apple-darwin"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/Xuepoo/carryctx/releases/download/v0.0.1/carryctx-x86_64-apple-darwin"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/Xuepoo/carryctx/releases/download/v0.0.1/carryctx-aarch64-unknown-linux-gnu"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/Xuepoo/carryctx/releases/download/v0.0.1/carryctx-x86_64-unknown-linux-gnu"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    if OS.mac?
+      if Hardware::CPU.arm?
+        bin.install "carryctx-aarch64-apple-darwin" => "carryctx"
+      else
+        bin.install "carryctx-x86_64-apple-darwin" => "carryctx"
+      end
+    elsif OS.linux?
+      if Hardware::CPU.arm?
+        bin.install "carryctx-aarch64-unknown-linux-gnu" => "carryctx"
+      else
+        bin.install "carryctx-x86_64-unknown-linux-gnu" => "carryctx"
+      end
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/carryctx --version")
+  end
+end

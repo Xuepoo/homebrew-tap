1dad5e73c882208b0f15513ffa0afbea37908f60
Xuepoo (CI) <xuepoofoter@gmail.com>
2026-07-31 15:10:25 +0000

    Update Topoglyph to 0.2.0

--- Formula/topoglyph.rb
+++ Formula/topoglyph.rb
@@ -1,14 +1,14 @@
 class Topoglyph < Formula
   desc "Topology-to-glyph text-art rendering engine"
   homepage "https://github.com/Xuepoo/topoglyph"
-  version "0.1.0"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Xuepoo/topoglyph/releases/download/v0.1.0/topoglyph-macos-x86_64"
-    sha256 "3a0659fe881b96e6cb9bdbdfa3a06f225468aefdebc6d5d8123e8fc63c54a302"
+    url "https://github.com/Xuepoo/topoglyph/releases/download/v0.2.0/topoglyph-macos-x86_64"
+    sha256 "6ba54de21e243d47fa0af1460494d4f8598ba899c3bc0edccdfb8b0f0e1513d1"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Xuepoo/topoglyph/releases/download/v0.1.0/topoglyph-macos-aarch64"
-    sha256 "60c210146985495eb49a656c3e8f8804157788e12208cab982c7307517e869e0"
+    url "https://github.com/Xuepoo/topoglyph/releases/download/v0.2.0/topoglyph-macos-aarch64"
+    sha256 "c3f146421b52b591a135fb5cd7c818a912bc413bdf988695b1e57838b0cf03d8"
   end
 
   def install

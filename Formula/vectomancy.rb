673fa4aaae10843b07b2d98f4b371e938a2ed25c
Xuepoo (CI) <xuepoofoter@gmail.com>
2026-07-31 03:12:07 +0000

    Update Vectomancy to 7.1.3

--- Formula/vectomancy.rb
+++ Formula/vectomancy.rb
@@ -1,14 +1,14 @@
 class Vectomancy < Formula
   desc "Image to vector math CLI"
   homepage "https://github.com/Xuepoo/vectomancy"
-  version "7.1.2"
+  version "7.1.3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Xuepoo/vectomancy/releases/download/v7.1.2/vectomancy-macos-x86_64"
-    sha256 "11e1001031cb590ea54c9927964906faaa1b6d8141679e082ebf1a9fd7501144"
+    url "https://github.com/Xuepoo/vectomancy/releases/download/v7.1.3/vectomancy-macos-x86_64"
+    sha256 "14a5efdce0b91d724361f40d109634516c2366c1de452660632dff0f921c7c86"
   elsif OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Xuepoo/vectomancy/releases/download/v7.1.2/vectomancy-macos-aarch64"
-    sha256 "3987039b8468c863dcd23333aca5f9ae4c4d68564bf488727ef03b5e76e44c64"
+    url "https://github.com/Xuepoo/vectomancy/releases/download/v7.1.3/vectomancy-macos-aarch64"
+    sha256 "3436551d725852e3b2b8808a0aca71479d49ff12aaebca0f35b17a46011d9f63"
   end
 
   def install
